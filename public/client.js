65ef7cb89cf5a8d922dd91f1b7be1f0621409930
Corso <Corso441@gmail.com>
2017-04-20 00:43:02 +0300

    Publik

--- public/client.js
+++ public/client.js
@@ -1,6 +1,12 @@
 $('#uploadForm').submit(function(e) {
     e.preventDefault();
 
+$(this).ajaxSubmit({
+    success: function(response) {
+         console.log('image uploaded and form submitted');
+    }
+});
+ 
 
     $.ajax({
         url: "/upload",
